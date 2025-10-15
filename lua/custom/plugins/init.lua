34b276cb2a88745e671763934a5c99e4845f6ff1
jutach88-ops <jutach88@gmail.com>
2025-10-15 00:20:56 -0400

    modified:   init.lua 	modified:   lua/custom/plugins/init.lua
    	modified:   lua/custom/plugins/init.lua
    	lua/custom/plugins/dap.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,58 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+-- bootstrap lazy.nvim (si ce n'est pas déjà fait)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Plugins
+require("lazy").setup({
+  -- Lazy gère lui-même
+  "folke/lazy.nvim",
+
+  -- Dépendances utiles
+  "nvim-lua/plenary.nvim",
+  "nvim-treesitter/nvim-treesitter",
+
+  -- Neotest principal
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("neotest").setup({})
+    end,
+  },
+
+  -- Adapter pour .NET
+  {
+    "nvim-neotest/neotest-dotnet",
+    dependencies = { "nvim-neotest/neotest" },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-dotnet")({
+            -- configuration optionnelle
+            -- args = { "--filter", "Category=Unit" },
+          }),
+        },
+      })
+    end,
+  },
+})
+
+
+
 return {}
